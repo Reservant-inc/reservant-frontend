815b701aa4d84093d506ff3b7e7e73d6c1d9846d
s26229 <s26229@pjwstk.edu.pl>
2024-03-12 22:25:17 +0100

    Added navigation from landing page to registration and login

--- reservant-frontend/src/components/landingPage/Home.tsx
+++ reservant-frontend/src/components/landingPage/Home.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Home () {
+    const navigate = useNavigate();
+
+    //navigation to registration
+    const handleRegisterClick = () => {
+        navigate("/register");
+    };
+
+    //navigation to login
+    const handleLoginClick = () => {
+        navigate("/login");
+    };
     return (
-        <></>
+        <>
+            <h1>Reservant</h1>
+            <button onClick={handleRegisterClick}>Zarejestruj się</button>
+            <button onClick={handleLoginClick}>Zaloguj się</button>
+        </>
     )
 }
