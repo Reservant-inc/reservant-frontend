5ad24b7192c9a64d9fdca47cf706690563387b3a
czapkazdaszkiem <jaroslaw.gawrych@gmail.com>
2024-04-01 19:32:58 +0200

    feat: osTheme + fix: theme button name
    added os preffered theme option + suggested (fixed?) different way to display theme buttons

--- src/components/ThemeButton.tsx
+++ src/components/ThemeButton.tsx
@@ -2,19 +2,31 @@ import React, {useState} from "react";
 
 function ThemeButton() {
 
-
-    const [isDark, setIsDark] = useState<boolean>(false);
-    
-
     //simple function for changing theme
   const toggleTheme = () => {
     document.documentElement.classList.toggle("dark");
-    setIsDark(!isDark);
+    console.log(document.documentElement.className==="dark")
+  };
+
+
+      //simple function for changing theme according to users preffered os settings
+  const OSTheme = () => {
+    if(window.matchMedia('(prefers-color-scheme: dark)').matches && !(document.documentElement.className==="dark"))
+    {
+      document.documentElement.classList.toggle("dark");
+    }
+
   };
 
   return (
-    //TODO changing icons instead of text
-      <button onClick={toggleTheme} className="bg-black text-white dark:bg-white dark:text-black"> {`${isDark?"light mode":"dark mode"}`} </button>
+    //TODO strong suggestion to make it list like with language choice, but with icons so its cooler
+    <>
+      <button onClick={toggleTheme} className="bg-black text-white dark:bg-white dark:text-black"> dark mode </button>
+      <br/>
+      <button onClick={toggleTheme} className="bg-black text-white dark:bg-white dark:text-black"> light mode </button>
+      <br/>
+      <button onClick={OSTheme} className="bg-black text-white dark:bg-white dark:text-black"> osTheme </button>
+    </>
   );
 }
 
