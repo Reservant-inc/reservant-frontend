07f23ba6939b957021d6523c0d7d3fc251a9367a
czapkazdaszkiem <jaroslaw.gawrych@gmail.com>
2024-04-16 06:46:22 +0200

    feat: role checkbox + restructure
    role checkbox - will be used for adding emp to restaurant. Added navigation, languages and moved file

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,7 +1,7 @@
 import LandingPage from "../landingPage/LandingPage"
 import Login from "../auth/login/Login"
 import RestaurantRegister from "../restaurant/register/RestaurantRegister"
-import EmployeeRegister from "../restaurant/EmployeeRegister/EmployeeRegister"
+import EmployeeRegister from "../auth/EmployeeRegister/EmployeeRegister"
 import React from "react"
 import Register from "../auth/register/Register"
 import HomePage from "../HomePage"
@@ -12,5 +12,5 @@ export const nav = [
      { path:     "/user/register",          element: <Register />,             isPrivate: false  },
      { path:     "/restaurant/register",    element: <RestaurantRegister />,   isPrivate: true   },
      { path:     "/home",                   element: <HomePage />,             isPrivate: true   },
-     {path: "/emp", element: <EmployeeRegister/>, isPrivate: false}
+     {path: "/addEmp", element: <EmployeeRegister/>, isPrivate: false}
 ]
