5fb14c212d0060b46e620aaa215b6689175b6558
Jarosław Gawrych <120335960+JaroslawGawrych@users.noreply.github.com>
2024-04-17 08:59:45 +0200

    Feat jg register emp

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,6 +1,7 @@
 import LandingPage from "../landingPage/LandingPage"
 import Login from "../auth/login/Login"
 import RestaurantRegister from "../restaurant/register/RestaurantRegister"
+import EmployeeRegister from "../auth/EmployeeRegister/EmployeeRegister"
 import React from "react"
 import Register from "../auth/register/Register"
 import HomePage from "../HomePage"
@@ -13,4 +14,6 @@ export const nav = [
      { path:     "/restaurant/register",    element: <RestaurantRegister />,   isPrivate: true,          roles: ["Customer", "RestaurantOwner"]  },
      { path:     "/home",                   element: <HomePage />,             isPrivate: true,          roles: ["Customer", "RestaurantOwner"]  },
      { path:     "/:user/restaurants",      element: <RestaurantManager />,    isPrivate: true,          roles: ["RestaurantOwner"]              },
+     { path:     "/emp",                    element: <EmployeeRegister/>,      isPrivate: true,          roles: ["RestaurantOwner"]              }
+    
 ]
