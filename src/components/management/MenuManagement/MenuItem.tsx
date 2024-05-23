56ca2282a24a8eae282b7cfa6e96e42ab01ae919
Miriam Staśkiewicz <miriam@MacBook-Air-Miriam.local>
2024-05-23 14:10:05 +0200

    MenuItem changed

--- src/components/management/MenuManagement/MenuItem.tsx
+++ src/components/management/MenuManagement/MenuItem.tsx
@@ -2,32 +2,39 @@ import React from "react";
 import { Card, CardContent } from "@mui/material";
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
+import DefaultMenuItem from '../../../assets/images/defaultMenuItemImage.png'
+import DefaultDrinkItem from '../../../assets/images/defaultDrinkItemImage.png';
+import { useTranslation } from "react-i18next";
 
 interface MenuItemProps {
     name: string;
     price: number;
     alcoholPercentage: number;
+    menuType: string; 
     onDelete: () => void;
     onEdit: () => void;
 }
 
-const MenuItem: React.FC<MenuItemProps> = ({ name, price, alcoholPercentage, onDelete, onEdit }) => {
+const MenuItem: React.FC<MenuItemProps> = ({ name, price, alcoholPercentage, menuType, onDelete, onEdit }) => {
+    const { t } = useTranslation("global");
+    const imagePath = menuType === 'Alcohol' ? DefaultDrinkItem : DefaultMenuItem;
+    const alcoholPercenrageVisable = menuType === 'Alcohol' ? true : false;
+    
     return (
         <Card className="w-64 dark:bg-grey-4 dark:text-grey-1 m-1">
-            <CardContent>
-                <div className="flex justify-between">
-                    <h2 className="text-lg font-medium">{name}</h2>
-                    <div>
+            <CardContent className="flex flex-col">
+                <img src={imagePath} alt="DefaultMenuItem" className="w-full h-auto mb-2" />
+                <div className="flex justify-between items-start">
+                    <h2 className="text-lg font-medium flex-grow">{name}</h2>
+                    <div className="flex space-x-1">
                         <button
-                            id="MenuItemEditButton"
-                            className="mr-1 rounded-lg bg-primary-2 p-1 w-8 h-8 dark:bg-secondary-2 dark:hover:bg-secondary dark:text-black"
+                            className="rounded-lg bg-primary-2 p-1 w-8 h-8 dark:bg-secondary-2 dark:hover:bg-secondary dark:text-black"
                             onClick={onEdit}
                         >
                             <EditIcon />
                         </button>
                         <button
-                            id="MenuItemDeleteButton"
-                            className="mr-1 rounded-lg bg-primary-2 p-1 w-8 h-8 dark:bg-secondary-2 dark:hover:bg-secondary dark:text-black"
+                            className="rounded-lg bg-primary-2 p-1 w-8 h-8 dark:bg-secondary-2 dark:hover:bg-secondary dark:text-black"
                             onClick={onDelete}
                         >
                             <DeleteIcon />
@@ -35,7 +42,7 @@ const MenuItem: React.FC<MenuItemProps> = ({ name, price, alcoholPercentage, onD
                     </div>
                 </div>
                 <p className="text-sm">{price} PLN</p>
-                <p className="text-xs">Alcohol Percentage: {alcoholPercentage}</p>
+                {(alcoholPercenrageVisable) && <p className="text-xs">{t("restaurant-management.menu.menuItemAlcoholPercentage")}: {alcoholPercentage}</p>}
             </CardContent>
         </Card>
     );
