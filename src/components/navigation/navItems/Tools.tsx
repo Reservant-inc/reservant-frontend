aa61330d1d7a5973698fff598e3a25a6e73e022a
Jarosław Gawrych <jaroslaw.gawrych@gmail.com>
2024-05-24 20:35:44 +0200

    cleanup

--- src/components/navigation/navItems/Tools.tsx
+++ src/components/navigation/navItems/Tools.tsx
@@ -7,8 +7,6 @@ import { CSSTransition } from "react-transition-group";
 import i18next from "i18next";
 import { ThemeProvider } from "@emotion/react";
 
-
-
 export interface ToolsProps {
     setIsDark: Function
 }
@@ -83,6 +81,7 @@ const Tools: React.FC<ToolsProps> = ({setIsDark}) => {
             const eqPos = cookie.indexOf("=");
             const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
             document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+            
         }
     }
 
