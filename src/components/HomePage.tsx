be769d68c1f77fc2bf45e985d919a9ba0d7f2a56
KubaBili001 <kubabili.wawa@gmail.com>
2024-07-11 14:51:36 +0200

    home page restaurant list style

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -15,10 +15,11 @@ import SearchIcon from '@mui/icons-material/Search';
 import StarPurple500SharpIcon from '@mui/icons-material/StarPurple500Sharp';
 import LocalOfferSharpIcon from '@mui/icons-material/LocalOfferSharp';
 import FocusedRestaurantDetails from "./restaurant/view/FocusedRestaurantDetails";
-import { fetchGET } from "../services/APIconn";
+import { fetchGET, getImage } from "../services/APIconn";
 import OutsideClickHandler from "./reusableComponents/OutsideClickHandler";
 
 export default function HomePage() {
+  //change from any once backend finishes the API endpoint
   const [restaurants, setRestaurants] = useState<any[]>([]);
   const [activeRestaurant, setActiveRestaurant] = useState<any>(null);
   const [loadedRestaurantIds, setLoadedRestaurantIds] = useState<Set<number>>(new Set());
@@ -26,6 +27,8 @@ export default function HomePage() {
   const [isReviewFilterPressed, setIsReviewFilterPressed] = useState<boolean>(false)
   const [isTagFilterPressed, setIsTagFilterPressed] = useState<boolean>(false)
   const [reviewFilter, setReviewFilter] = useState<number>(0)
+  const [tags, setTags] = useState<string[]>([])
+  const [chosenTags, setChosenTags] = useState<number[]>([])
 
   //center of warsaw, cant get users location without https
   const [bounds, setBounds] = useState<any>({
@@ -40,8 +43,7 @@ export default function HomePage() {
       try {
         const response = await fetchGET(`/restaurants/in-area?lat1=${bounds.lat1}&lon1=${bounds.lon1}&lat2=${bounds.lat2}&lon2=${bounds.lon2}`);
         const newRestaurants = response.filter((restaurant: any) => !loadedRestaurantIds.has(restaurant.restaurantId));
-         
-        console.log(newRestaurants.length)
+        console.log(response)
         if (newRestaurants.length > 0) {
           setRestaurants([...restaurants, ...newRestaurants]);
           setLoadedRestaurantIds(prev => {
@@ -57,6 +59,18 @@ export default function HomePage() {
     getRestaurants();
   }, [bounds]);
 
+  useEffect(() => {
+    const getRestaurants = async () => {
+      try {
+        const response = await fetchGET('/restaurant-tags');
+        setTags(response)
+      } catch (error) {
+        console.error("Error getting restaurants", error);
+      }
+    };
+    getRestaurants();
+  }, [])
+
   const reviewsPressHandler = () => {
     setIsReviewFilterPressed(!isReviewFilterPressed)
   }
@@ -67,33 +81,54 @@ export default function HomePage() {
 
   return (
     <div id="homePage-wrapper" className="relative flex h-[calc(100%-3.5rem)] w-full bg-grey-1 dark:bg-grey-3">
-      <div id="homePage-restaurantList-wrapper" className="h-full w-[17.5%] bg-white shadow-md p-3">
-      <InputBase
-        placeholder="Search for restaurants"
-      />
-      <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
-        <SearchIcon />
-      </IconButton>
+      <div id="homePage-restaurantList-wrapper" className="h-full w-[20%] min-w-[300px] bg-white shadow-md overflow-y-scroll scroll">
+          <div className="p-3">
+            <div className="w-full flex px-2 rounded-full border-[1px] border-grey-2">
+            <input 
+              type="text"
+              placeholder="Search"
+              className="w-full clean-input"
+            />
+            <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+              <SearchIcon />
+            </IconButton>
+          </div>
+        </div>
         <List
           id="homePage-restaurantList"
           className="font-mont-md dark:bg-black w-full"
           component="nav"
-          subheader={
-            <ListSubheader component="div" id="nested-list-subheader">
-              Restaurants near you
-            </ListSubheader>
-          }
         >
           {restaurants.map((restaurant, index) => (
-            <ListItemButton id="homePage-listItemButton"
-              onClick={() => {
-                setUserMovedMap(false)
-                setActiveRestaurant(restaurant)
-              }}
-              key={index}
-            >
-              <ListItemText id="homePage-listItemText" primary={restaurant.name} />
-            </ListItemButton>
+            <>
+              <ListItemButton id="homePage-listItemButton"
+                onClick={() => {
+                  setUserMovedMap(false)
+                  setActiveRestaurant(restaurant)
+                }}
+                key={index}
+                className={activeRestaurant === restaurant ? "bg-grey-1" : "bg-white"}
+              >
+                <div className="flex items-start justify-between w-full py-3">
+                  <div className="flex flex-col gap-1">
+                    <h1 className="font-mont-md text-md">{restaurant.name}</h1>
+                    <div className="flex">
+                      <h1 className="text-sm">5</h1>
+                      <Rating name="read-only" value={5} readOnly className="text-[18px]"/>
+                    </div>
+                    <h1 className="text-sm font-mont-l">{restaurant.address}</h1>
+                    <h1 className="text-sm font-mont-l">{restaurant.provideDelivery ? "Provides delivery" : "No delivery"}</h1>
+                    {
+                      restaurant.tags.map((tag : string) => (
+                        <h1>{tag}</h1>
+                      ))
+                    }
+                  </div>
+                  <img src={getImage(restaurant.logo)} alt="logo" className="h-24 w-24 rounded-lg"/>
+                </div>
+              </ListItemButton>
+              <div className="w-full h-0 border-b-[1px] border-grey-1"></div>
+            </>
           ))}
         </List>
       </div>
@@ -113,7 +148,7 @@ export default function HomePage() {
           userMovedMap={userMovedMap}
         />
       </div>
-      <div className="absolute left-[calc(17.5%+15px)] h-[40px] w-[450px] top-[15px] flex items-center gap-4">
+      <div className="absolute min-left-[300px] left-[20%] h-[40px] w-[450px] top-[15px] flex items-center gap-4">
           <OutsideClickHandler onOutsideClick={reviewsPressHandler} isPressed={isReviewFilterPressed}>
             <Button id="homePage-reviewsFilter" className={"h-full rounded-lg shadow-md p-2 flex gap-2 " + (reviewFilter != 0 ? "bg-primary text-white" : "bg-white text-black")}
               onClick={reviewsPressHandler}
@@ -139,7 +174,7 @@ export default function HomePage() {
                     >
                       <Typography component="legend">Any rating</Typography>
                     </ListItemButton>
-                    {[1, 2, 3, 4, 5].map((value) => (
+                    {[2, 3, 4, 5].map((value) => (
                       <ListItemButton
                         id="homePage-listItemButton"
                         className="flex gap-2 justify-center items-center"
@@ -158,15 +193,34 @@ export default function HomePage() {
               )}
           </OutsideClickHandler>
           <OutsideClickHandler onOutsideClick={tagsPressHandler} isPressed={isTagFilterPressed}>
-            <Button id="homePage-tagssFilter" className={"h-full rounded-lg shadow-md p-2 flex gap-2 bg-white text-black"}
+            <Button id="homePage-tagssFilter" className={"h-full rounded-lg shadow-md p-2 flex gap-2 " + (chosenTags.length > 0 ? "bg-primary text-white" : "bg-white text-black")}
               onClick={tagsPressHandler}
             >
               <LocalOfferSharpIcon className="h-6"/>
-              Tags
+              {   
+              chosenTags.length > 0 ? chosenTags.length === 1 ? `${chosenTags.length} tag` : `${chosenTags.length} tags` : "Tags"
+              } 
             </Button>
               {isTagFilterPressed && (
-                <div className="absolute top-[55px] bg-white z-[2] shadow-2xl rounded-lg w-24 h-24">
-
+                <div className="absolute top-[55px] bg-white z-[2] shadow-2xl rounded-lg w-44 flex flex-wrap justify-start p-3 gap-2">
+                  {
+                    tags.map((tag, i) => (
+                      <button
+                        key={i}
+                        className={`rounded-full border-[1px] border-grey-2 p-2 text-[12px] font-mont-md ${chosenTags.includes(i) ? "text-white bg-primary" : "text-black bg-white"}`}
+                        onClick={() => {
+                          if (chosenTags.includes(i)) {
+                            const updatedTags = chosenTags.filter(tagIndex => tagIndex !== i);
+                            setChosenTags(updatedTags);
+                          } else {
+                            setChosenTags([...chosenTags, i]);
+                          }
+                        }}
+                      >
+                        {tag.toUpperCase()}
+                      </button>
+                    ))
+                  }
                 </div>  
               )}
           </OutsideClickHandler>
