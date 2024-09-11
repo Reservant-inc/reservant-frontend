654b55a1f077b2be74d3ced3b1ccce831f7dc39d
s26496 <s26496@pjwstk.edu.pl>
2024-09-11 15:58:54 +0200

    Warehouse management
    List displaying ingredients for a restaurant
    Functionaltiy to generate grocery list and edit it, then place an order
    Functionality to add a new ingredient for a restaurant

--- src/components/restaurantManagement/Dashboard/RestaurantDashboardSection.tsx
+++ src/components/restaurantManagement/Dashboard/RestaurantDashboardSection.tsx
@@ -2,10 +2,8 @@ import React from "react";
 
 const RestaurantDashboardSection = () => {
   return (
-    <div className="h-full w-full bg-white rounded-lg shadow-md">
-      Dash
-    </div>
-  )
+    <div className="h-full w-full rounded-lg bg-white shadow-md">Dash</div>
+  );
 };
- 
+
 export default RestaurantDashboardSection;
