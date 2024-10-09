8e96d57092261409b20acaf8c0a827c0b9fbcfea
Jarosław Gawrych <jaroslaw.gawrych@gmail.com>
2024-10-09 19:27:54 +0200

    css: text inputs in menu creator

--- src/components/restaurantManagement/menus/MenuDialog.tsx
+++ src/components/restaurantManagement/menus/MenuDialog.tsx
@@ -11,6 +11,7 @@ import { MenuItemType, MenuType } from "../../../services/types";
 import { FetchError } from "../../../services/Errors";
 import MenuItem from "./newMenus/MenuItem";
 import {MenuScreenType} from "../../../services/enums"
+import ErrorMes from "../../reusableComponents/ErrorMessage";
 
 interface MenuDialogProps {
   onClose: () => void;
@@ -162,176 +163,210 @@ const MenuDialog: React.FC<MenuDialogProps> = ({
 
 
   return (
-      <div className="flex">
-        <div>
-          <Formik
-            initialValues={{ 
-              name: menu?menu.name:"",
-              alternateName: menu?menu.alternateName:"",
-              menuType: menu?menu.menuType:"",
-              dateFrom: menu?menu.dateFrom:"",
-              dateUntil: menu?menu.dateUntil:"",
-            }} 
-            onSubmit={menu?handleEditedNewMenu:handleSaveNewMenu}
-            validationSchema={menuSchema}
-          >
-            {(formik) => {
-              return(
-                <Form>
+    <div className=" flex h-[90vh] w-[50vw] min-w-[950px] bg-white rounded-lg dark:bg-black p-7">
+      <div>
+        <Formik
+          initialValues={{ 
+            name: menu?menu.name:"",
+            alternateName: menu?menu.alternateName:"",
+            menuType: menu?menu.menuType:"",
+            dateFrom: menu?menu.dateFrom:"",
+            dateUntil: menu?menu.dateUntil:"",
+          }} 
+          onSubmit={menu?handleEditedNewMenu:handleSaveNewMenu}
+          validationSchema={menuSchema}
+        >
+          {(formik) => {
+            return(
+              <Form>
+                <div className="flex flex-col gap-7">
                   <div>
-                    <Field 
-                      type="text" 
-                      id="name" 
-                      name="name"
-                      className={` w-full [&>*]:before:border-0 [&>*]:after:border-0  ${!(formik.errors.name && formik.touched.name) ? "[&>*]:text-black " : "[&>*]:text-error" }`} 
-                      variant="standard"
-                      label="name"
-                      as={TextField}
-                      //@TODO translation
-                    />
-                    <Field 
-                      type="text" 
-                      id="alternateName" 
-                      name="alternateName"
-                      className={` w-full [&>*]:before:border-0 [&>*]:after:border-0  ${!(formik.errors.alternateName && formik.touched.alternateName) ? "[&>*]:text-black " : "[&>*]:text-error" }`} 
-                      variant="standard"
-                      label="alternateName"
-                      as={TextField}
-                      //@TODO translation
-                    />
-                    <Field 
-                      id="menuType" 
-                      name="menuType"
-                      className={` w-full [&>*]:before:border-0 [&>*]:after:border-0  ${!(formik.errors.menuType && formik.touched.menuType) ? "[&>*]:text-black " : "[&>*]:text-error" }`} 
-                      label="menuType"
-                      as={"select"}
-                      //@TODO translation
-                    >
-                      <option value="" id="MenuMenuType-option-default">Select a menu type</option>
-                      {/* //@TODO translation */}
-                      {
-                        menuTypes.map((menuType) => 
-                        <option value={menuType}> 
-                          {menuType} 
-                        </option>)
-                      }
-                    </Field>
-                    <Field 
-                      type="date" 
-                      id="dateFrom" 
-                      name="dateFrom"
-                      className={` w-full [&>*]:before:border-0 [&>*]:after:border-0  ${!(formik.errors.dateFrom && formik.touched.dateFrom) ? "[&>*]:text-black " : "[&>*]:text-error" }`} 
-                      variant="standard"
-                      label="dateFrom"
-                      //@TODO translation
-                    />
-                    <Field 
-                      type="date" 
-
-                      id="dateUntil" 
-                      name="dateUntil"
-                      className={` w-full [&>*]:before:border-0 [&>*]:after:border-0  ${!(formik.errors.dateUntil && formik.touched.dateUntil) ? "[&>*]:text-black " : "[&>*]:text-error" }`} 
-                      label="dateUntil"
-                      //@TODO translation
-                    />
-                    <button 
-                      id="addmenuSubmit"
-                      type="submit"
-                      disabled={!formik.isValid || (!formik.dirty && menu===null) }
-                      className={"shadow h-12 min-w-1/2 w-48 justify-center items-center gap-2 flex rounded-lg p-1 dark:bg-grey-5 bg-grey-0 dark:text-secondary text-primary dark:text-secondary enabled:dark:hover:bg-secondary enabled:dark:hover:text-black enabled:hover:text-white enabled:hover:bg-primary  " }
-                    >
-                      <Save/>
-                      {t("general.save")}
-                    </button>
-                  </div>
-                </Form>
-              )
-            }}
-          </Formik>
-        </div>
-        <div className="flex flex-col  items-start h-full w-full gap-6">
-            <div className=" w-full ">
-              <Formik  
-                  initialValues={{id: ""}} 
-                  onSubmit={addMenuItem}
-                  validationSchema={menuItemSelectorSchema}
-                >
-                  {(formik) => {
-                    return(
-                      <Form>
-                        <div className="flex  justify-center w-full">
-                          <Field
-                            as={"select"}
-                            id="id" 
-                            name="id" 
-                            label="id"
-                            className="w-1/3 border-0 border-b"
-                          >
-                            <option value="" id="MI-option-default">Select a menu item</option>
-                              {/* @todo t  */}
-                              {menuItems
-                              .filter(menuItem=>!menu?.menuItems.find(activeMI=>activeMI.menuItemId==menuItem.menuItemId))
-                              .filter(menuItem=>!selectedMenuItems.find(selectedMI=>selectedMI.menuItemId==menuItem.menuItemId))
-                              .map((menuItem) => 
-                              <option value={menuItem.menuItemId}> 
-                                {menuItem.name}
-                              </option>
-                              )}
-                          </Field>
-                          <button
-                            type="submit"
-                            className={`  border-b  text-grey-black  ${formik.isValid&&formik.dirty?` hover:text-primary`:``}  ` }
-                            id="addMenu"
-                            disabled={!formik.isValid || !formik.dirty}
-                          >
-                            <Add/>
-                          </button>
-                          <button
-                            disabled={selectedMenuItems.length<=0}
-                            onClick={()=>{
-                              setSelectedMenuItems([]);
-                            }}
-                            className={` shadow w-48 h-12 rounded-lg p-1 dark:bg-grey-5 bg-grey-0 dark:text-secondary text-primary dark:text-secondary enabled:dark:hover:bg-secondary enabled:dark:hover:text-black enabled:hover:text-white enabled:hover:bg-primary ` }
-                          >
-                              Clear all items
-                              {/* @TODO translation */}
-                          </button>
-                        </div>
-                      </Form>
-                    )
-                  }}
-                </Formik>
-              </div>
-              <div className="flex w-full h-[70%]  items-center gap-6 flex-col">
-                <div className=" shadow-inner bg-grey-0 rounded-lg overflow-y-auto p-1  w-full  h-full   ">
-                  {selectedMenuItems.length>0
-                  ?
-                  <ul
-                    className="flex h-full  w-full flex-wrap "  
-                  > 
+                    <div className={`flex items-center justify-start gap-1 border-b-[1px] text-nowrap ${formik.errors.name&&formik.touched.name?"border-error text-error":"border-black text-black"}`}>
+                      <label htmlFor="name">Name:</label>
+                      <Field 
+                        type="text" 
+                        id="name" 
+                        name="name"
+                        className="w-full"
+                        //@TODO translation
+                      />
+                    </div>
                     {
-                      selectedMenuItems.map((menuItem: MenuItemType) => 
-                        <li key={menuItem.menuItemId}>
-                          <MenuItem
-                            key={menuItem.menuItemId}
-                            type={MenuScreenType.Preview}
-                            menuItem={menuItem}
-                            menu={menu}
-                            activeRestaurantId={activeRestaurantId}
-                        />
-                        </li>
-                      )
+                      (formik.errors.name&&formik.touched.name) &&
+                      <ErrorMes msg={formik.errors.name}/>
                     }
-                  </ul>
-                  :
-                  <h1 className="p-2">Selected menu items will appear here.</h1> //@TODO translation 
-                  }
+                  </div>
+                  <div>
+                    <div className={`flex items-center justify-start gap-1 border-b-[1px] text-nowrap ${formik.errors.alternateName&&formik.touched.alternateName?"border-error text-error":"border-black text-black"}`}>
+                      <label htmlFor="alternateName">Alternate name:</label>
+                      <Field 
+                        type="text" 
+                        id="alternateName" 
+                        name="alternateName"
+                        className="w-full"
+                        //@TODO translation
+                      />
+                    </div>
+                    {
+                      (formik.errors.alternateName&&formik.touched.alternateName) &&
+                      <ErrorMes msg={formik.errors.alternateName}/>
+                    }                  
+                  </div>
+                  <div>
+                    <div className={`flex items-center justify-start gap-1 border-b-[1px] text-nowrap ${formik.errors.menuType&&formik.touched.menuType?"border-error text-error":"border-black text-black"}`}>
+                      <label htmlFor="menuType">Menu type:</label>
+                      <Field 
+                        id="menuType" 
+                        name="menuType"
+                        as={"select"}
+                        className="border-none w-full"
+                        //@TODO translation
+                      >
+                        <option className="text-black" value="" id="MenuMenuType-option-default">Select a menu type</option>
+                        {/* //@TODO translation */}
+                        {
+                          menuTypes.map((menuType) => 
+                          <option className="text-black" value={menuType}> 
+                            {menuType} 
+                          </option>)
+                        }
+                      </Field>
+                      </div>
+                      {
+                        (formik.errors.menuType&&formik.touched.menuType) &&
+                        <ErrorMes msg={formik.errors.menuType}/>
+                      }        
+                  </div>
+                  <div>
+                    <div className={`flex items-center justify-start gap-1 border-b-[1px] text-nowrap ${formik.errors.dateFrom&&formik.touched.dateFrom?"border-error text-error":"border-black text-black"}`}>
+                      <label htmlFor="dateFrom">Active since:</label>
+                      <Field 
+                        type="date" 
+                        id="dateFrom" 
+                        name="dateFrom"
+                        className="w-full"
+                        //@TODO translation
+                      />
+                    </div>
+                    {
+                      (formik.errors.dateFrom&&formik.touched.dateFrom) &&
+                      <ErrorMes msg={formik.errors.dateFrom}/>
+                    }                  
+                  </div>
+                  <div>
+                    <div className={`flex items-center justify-start gap-1 border-b-[1px] text-nowrap ${formik.errors.dateUntil&&formik.touched.dateUntil?"border-error text-error":"border-black text-black"}`}>
+                      <label htmlFor="dateUntil">Active until:</label>
+                      <Field 
+                        type="date" 
+                        id="dateUntil" 
+                        name="dateUntil"
+                        className="w-full"
+                        //@TODO translation
+                      />
+                    </div>
+                    {
+                      (formik.errors.dateUntil&&formik.touched.dateUntil) &&
+                      <ErrorMes msg={formik.errors.dateUntil}/>
+                    }   
+                  </div>
+                  <button 
+                    id="addmenuSubmit"
+                    type="submit"
+                    disabled={!formik.isValid || (!formik.dirty && menu===null) }
+                    className={"shadow self-center h-12 min-w-1/2 w-48 justify-center items-center gap-2 flex rounded-lg p-1 dark:bg-grey-5 bg-grey-0 dark:text-secondary text-primary dark:text-secondary enabled:dark:hover:bg-secondary enabled:dark:hover:text-black enabled:hover:text-white enabled:hover:bg-primary  " }
+                  >
+                    <Save/>
+                    {t("general.save")}
+                  </button>
                 </div>
-                
-                
-            </div>
-          </div>
+              </Form>
+            )
+          }}
+        </Formik>
       </div>
+      <div className="flex flex-col  items-start h-full w-full gap-6">
+          <div className=" w-full ">
+            <Formik  
+                initialValues={{id: ""}} 
+                onSubmit={addMenuItem}
+                validationSchema={menuItemSelectorSchema}
+              >
+                {(formik) => {
+                  return(
+                    <Form>
+                      <div className="flex  justify-center w-full">
+                        <Field
+                          as={"select"}
+                          id="id" 
+                          name="id" 
+                          label="id"
+                          className="w-1/3 border-0 border-b"
+                        >
+                          <option value="" id="MI-option-default">Select a menu item</option>
+                            {/* @todo t  */}
+                            {menuItems
+                            .filter(menuItem=>!menu?.menuItems.find(activeMI=>activeMI.menuItemId==menuItem.menuItemId))
+                            .filter(menuItem=>!selectedMenuItems.find(selectedMI=>selectedMI.menuItemId==menuItem.menuItemId))
+                            .map((menuItem) => 
+                            <option value={menuItem.menuItemId}> 
+                              {menuItem.name}
+                            </option>
+                            )}
+                        </Field>
+                        <button
+                          type="submit"
+                          className={`  border-b  text-grey-black  ${formik.isValid&&formik.dirty?` hover:text-primary`:``}  ` }
+                          id="addMenu"
+                          disabled={!formik.isValid || !formik.dirty}
+                        >
+                          <Add/>
+                        </button>
+                        <button
+                          disabled={selectedMenuItems.length<=0}
+                          onClick={()=>{
+                            setSelectedMenuItems([]);
+                          }}
+                          className={` shadow w-48 h-12 rounded-lg p-1 dark:bg-grey-5 bg-grey-0 dark:text-secondary text-primary dark:text-secondary enabled:dark:hover:bg-secondary enabled:dark:hover:text-black enabled:hover:text-white enabled:hover:bg-primary ` }
+                        >
+                            Clear all items
+                            {/* @TODO translation */}
+                        </button>
+                      </div>
+                    </Form>
+                  )
+                }}
+              </Formik>
+            </div>
+            <div className="flex w-full h-[70%]  items-center gap-6 flex-col">
+              <div className=" shadow-inner bg-grey-0 rounded-lg overflow-y-auto p-1  w-full  h-full   ">
+                {selectedMenuItems.length>0
+                ?
+                <ul
+                  className="flex h-full  w-full flex-wrap "  
+                > 
+                  {
+                    selectedMenuItems.map((menuItem: MenuItemType) => 
+                      <li key={menuItem.menuItemId}>
+                        <MenuItem
+                          key={menuItem.menuItemId}
+                          type={MenuScreenType.Preview}
+                          menuItem={menuItem}
+                          menu={menu}
+                          activeRestaurantId={activeRestaurantId}
+                      />
+                      </li>
+                    )
+                  }
+                </ul>
+                :
+                <h1 className="p-2">Selected menu items will appear here.</h1> //@TODO translation 
+                }
+              </div>
+              
+              
+          </div>
+        </div>
+    </div>
   );
 };
 
