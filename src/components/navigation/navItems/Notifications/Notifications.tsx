0875075a09ba2fec8af39663bf6863d9f678fe90
s26496 <127142274+s26496@users.noreply.github.com>
2024-10-30 10:11:22 +0100

    notifications functionality
    - Added notifications section
    - Custom notifications for each type (except NotificationNewFriendRequest bc its bugged now)

--- src/components/navigation/navItems/Notifications/Notifications.tsx
+++ src/components/navigation/navItems/Notifications/Notifications.tsx
@@ -3,6 +3,7 @@ import OutsideClickHandler from "../../../reusableComponents/OutsideClickHandler
 import { Notifications as NotificationsIcon } from "@mui/icons-material";
 import NotificationList from "./NotificationList";
 import { fetchGET } from "../../../../services/APIconn";
+import { Button } from "@mui/material";
 
 interface NotificationsButtonProps {
   isDark: boolean;
@@ -12,44 +13,51 @@ const NotificationsButton: React.FC<NotificationsButtonProps> = ({
   isDark,
 }) => {
   const [isPressed, setIsPressed] = useState<boolean>(false);
-  const [hasNotifications, setHasNotifications] = useState<boolean>(true);
+  const [unreadNotificationCount, setUnreadNotificationCount] = useState<number>(0);
   const [loading, setLoading] = useState<boolean>(true);
 
   const pressHandler = () => {
     setIsPressed(!isPressed);
   };
 
-  const fetchNotifications = async () => {
+  const fetchNotificationCount = async () => {
     setLoading(true);
     try {
-      const response = await fetchGET("/friends/incoming");
-      setHasNotifications(response.items.length > 0);
+      const response = await fetchGET("/notifications/bubbles");
+      setUnreadNotificationCount(response.unreadNotificationCount);
     } catch (error) {
-      console.error("Error fetching incoming friend requests:", error);
+      console.error("Error fetching unread notifications count:", error);
     } finally {
       setLoading(false);
     }
   };
 
+  const updateUnreadCount = async () => {
+    await fetchNotificationCount();
+  };
+
   useEffect(() => {
-    fetchNotifications();
+    fetchNotificationCount();
   }, []);
 
   return (
     <OutsideClickHandler onOutsideClick={pressHandler} isPressed={isPressed}>
-      <button
+      <Button
         id="NotificationsButton"
-        className="relative flex h-[40px] w-[40px] items-center justify-center rounded-full bg-grey-1 dark:bg-grey-5 dark:text-grey-1"
+        className={`relative flex h-[40px] w-[40px] min-w-[40px] items-center justify-center rounded-full bg-grey-1 dark:bg-grey-5 text-black dark:text-grey-1 ${isPressed && "text-primary dark:text-secondary"}`}
         onClick={pressHandler}
       >
-        <NotificationsIcon className="h-[30px] w-[30px]" />
-        {hasNotifications && !loading && (
-          <span className="absolute right-1 top-0 h-3 w-3 rounded-full bg-primary dark:bg-secondary"></span>
+        <NotificationsIcon className="h-[23px] w-[23px]" />
+        {/* Wyświetl span tylko jeśli są nieprzeczytane powiadomienia */}
+        {unreadNotificationCount > 0 && !loading && (
+          <span className="absolute right-[-2px] top-0 h-4 w-4 rounded-full bg-primary dark:bg-secondary text-white text-xs flex items-center justify-center">
+            {unreadNotificationCount}
+          </span>
         )}
-      </button>
+      </Button>
       {isPressed && (
-        <div className="nav-dropdown h-[150px] w-[300px] dark:bg-black">
-          <NotificationList setHasNotifications={setHasNotifications} />
+        <div className="nav-dropdown flex h-[calc(100%-4.5rem)] w-[300px] flex-col items-center z-[1] bg-white dark:bg-black">
+          <NotificationList updateUnreadCount={updateUnreadCount} />
         </div>
       )}
     </OutsideClickHandler>
