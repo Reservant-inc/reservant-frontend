563062c7b631f16d09b350166ab73f31ea81ad7c
KubaBili001 <kubabili.wawa@gmail.com>
2024-11-06 18:15:05 +0100

    routing bug fix

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -46,19 +46,19 @@ const Profile: React.FC = () => {
           )}
         </div>
         <button
-          className="border-[1px] rounded-lg p-1 border-primary text-primary transition hover:scale-105 hover:bg-primary hover:text-white dark:border-secondary dark:text-secondary dark:hover:bg-secondary dark:hover:text-black"
+          className="border-[1px] rounded-lg p-1 bg-grey-0 border-primary text-primary transition hover:scale-105 hover:bg-primary hover:text-white dark:border-secondary dark:text-secondary dark:hover:bg-secondary dark:hover:text-black"
           onClick={() => navigate("account")}
         >
           <h1 className="text-sm">Account</h1>
         </button>
         <button
-          className="border-[1px] rounded-lg p-1 border-primary text-primary transition hover:scale-105 hover:bg-primary hover:text-white dark:border-secondary dark:text-secondary dark:hover:bg-secondary dark:hover:text-black"
+          className="border-[1px] rounded-lg p-1 bg-grey-0 border-primary text-primary transition hover:scale-105 hover:bg-primary hover:text-white dark:border-secondary dark:text-secondary dark:hover:bg-secondary dark:hover:text-black"
           onClick={() => navigate("reservation-history")}
         >
           <h1 className="text-sm">Reservations</h1>
         </button>
         <button
-          className="border-[1px] rounded-lg p-1 border-primary text-primary transition hover:scale-105 hover:bg-primary hover:text-white dark:border-secondary dark:text-secondary dark:hover:bg-secondary dark:hover:text-black"
+          className="border-[1px] rounded-lg p-1 bg-grey-0 border-primary text-primary transition hover:scale-105 hover:bg-primary hover:text-white dark:border-secondary dark:text-secondary dark:hover:bg-secondary dark:hover:text-black"
           onClick={() => navigate("event-history")}
         >
           <h1 className="text-sm">Events</h1>
