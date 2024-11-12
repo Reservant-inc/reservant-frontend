c1eee9b2779769afe1dc942642440170db0eb96e
KubaBili001 <kubabili.wawa@gmail.com>
2024-11-12 18:20:14 +0100

    added transaction, style changes

--- src/components/restaurantManagement/restaurants/restaurantReviews/RestaurantReviewsFilters.tsx
+++ src/components/restaurantManagement/restaurants/restaurantReviews/RestaurantReviewsFilters.tsx
@@ -1,36 +1,41 @@
-import React from "react";
-import CustomRating from "../../../reusableComponents/CustomRating";
-import { useTranslation } from "react-i18next";
-import { Tooltip } from "@mui/material";
-import CloseIcon from "@mui/icons-material/Close";
+import React from 'react'
+import CustomRating from '../../../reusableComponents/CustomRating'
+import { useTranslation } from 'react-i18next'
+import { Tooltip } from '@mui/material'
+import CloseIcon from '@mui/icons-material/Close'
 
 interface RestaurantReviewsFiltersProps {
-  setValue: Function;
-  value: number;
+  setValue: Function
+  value: number
 }
 
 const RestaurantReviewsFilters: React.FC<RestaurantReviewsFiltersProps> = ({
   setValue,
-  value,
+  value
 }) => {
-  const [t] = useTranslation("global");
+  const [t] = useTranslation('global')
 
   return (
     <div>
       <div className="flex items-center gap-1 rounded-lg bg-grey-0 p-2 dark:bg-grey-5">
-        <h1 className="text-sm">{t("reviews.filter")}:</h1>
-        <CustomRating readOnly={false} rating={value} onChange={setValue} className="text-[20px]"/>
-        <Tooltip title={t("reviews.filter-tooltip")}>
+        <h1 className="text-sm">{t('reviews.filter')}:</h1>
+        <CustomRating
+          readOnly={false}
+          rating={value}
+          onChange={setValue}
+          className="text-[20px]"
+        />
+        <Tooltip title={t('reviews.filter-tooltip')}>
           <button
             className="h-6 w-6 bg-trans flex items-center justify-center hover:bg-grey-1 dark:hover:bg-grey-4 dark:text-grey-1 rounded-full"
             onClick={() => setValue(0)}
           >
-            <CloseIcon className="h-5 w-5"/>
+            <CloseIcon className="h-5 w-5" />
           </button>
         </Tooltip>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default RestaurantReviewsFilters;
+export default RestaurantReviewsFilters
