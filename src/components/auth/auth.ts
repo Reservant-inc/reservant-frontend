074a70730e0b1d9318f8d8c5b324d49f8296a856
KubaBili001 <kubabili.wawa@gmail.com>
2024-11-19 11:46:39 +0100

    fixed a bug in restaurant reviews

--- src/components/auth/auth.ts
+++ src/components/auth/auth.ts
@@ -1,26 +1,29 @@
-import Cookies from "js-cookie";
-import { UserInfo } from "../../services/types";
+import Cookies from 'js-cookie'
 
-  export function checkAuthLoader() {
-    const isLoggedIn = Boolean(Cookies.get("token"));
-    if (!isLoggedIn) {
-      throw new Response("", { status: 302, headers: { Location: "/login" } });
-    }
-    return null;
+export function checkAuthLoader() {
+  const isLoggedIn = Boolean(Cookies.get('token'))
+  if (!isLoggedIn) {
+    throw new Response('', { status: 302, headers: { Location: '/login' } })
   }
+  return null
+}
 
 export function redirectIfLoggedIn() {
-    const isLoggedIn = Boolean(Cookies.get("token"));
-    if (isLoggedIn) {
-      throw new Response("", { status: 302, headers: { Location: "/reservant" } });
-    }
-    return null;
+  const isLoggedIn = Boolean(Cookies.get('token'))
+  if (isLoggedIn) {
+    throw new Response('', { status: 302, headers: { Location: '/reservant' } })
   }
+  return null
+}
 
-  export function checkIfOwner() {
-    const isOwner = Boolean((JSON.parse(Cookies.get("userInfo") as string)).roles.includes("Restaurant Owner"))
-    if (isOwner) {
-      throw new Response("", { status: 302, headers: { Location: "/reservant" } });
-    }
-    return null;
-  } 
+export function checkIfOwner() {
+  const isOwner = Boolean(
+    JSON.parse(Cookies.get('userInfo') as string).roles.includes(
+      'Restaurant Owner'
+    )
+  )
+  if (isOwner) {
+    throw new Response('', { status: 302, headers: { Location: '/reservant' } })
+  }
+  return null
+}
