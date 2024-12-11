64ec9e323ac5c65435c224463cc2560dfa6a23e0
Jarosław Gawrych <jaroslaw.gawrych@gmail.com>
2024-12-11 09:55:57 +0100

    date format

--- src/components/reservant/profile/reports/Report.tsx
+++ src/components/reservant/profile/reports/Report.tsx
@@ -15,7 +15,6 @@ const Report: React.FC<ReportProps> = ({ report }) => {
       day: 'numeric',
       hour: '2-digit',
       minute: '2-digit',
-      second: '2-digit',
       hour12: false
     })
   }
