16785793e83125c3d33f146b5e665878b6ee827a
Miriam Staśkiewicz <miriam@MacBook-Air-Miriam.local>
2024-12-17 23:44:10 +0100

    error shows after 403 in login

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -35,6 +35,12 @@ const Login: React.FC = () => {
 
       const response = await fetchPOST('/auth/login', JSON.stringify(values))
 
+      if (response.status === 403) {
+        // Jeśli status to 403, ustaw błąd z komunikatem
+        setLoginError(t('landing-page.error-403'));
+        return;  // Zatrzymujemy dalsze przetwarzanie
+      }
+
       login(response)
     } catch (error) {
       if (error instanceof FetchError) {
