ab50991b538ba5273c4da36a5a6bcb22acd8aa74
Jarosław Gawrych <120335960+JaroslawGawrych@users.noreply.github.com>
2025-01-06 19:29:48 +0100

    created validation function and added *pretty* alert

--- src/components/reservant/profile/reservations/Reservation.tsx
+++ src/components/reservant/profile/reservations/Reservation.tsx
@@ -11,6 +11,10 @@ import { FetchError } from '../../../../services/Errors'
 import { format } from 'date-fns'
 import Dialog from '../../../reusableComponents/Dialog'
 import { ReservationListType } from '../../../../services/enums'
+import { Alert, IconButton } from '@mui/material'
+import CloseIcon from '@mui/icons-material/Close'
+import ErrorMes from '../../../reusableComponents/ErrorMessage'
+import { report } from 'node:process'
 
 interface ReservationProps {
   reservation: VisitType
@@ -27,6 +31,8 @@ const Reservation: React.FC<ReservationProps> = ({
   const [reportType, setReportType] = useState<string>('')
   const [reportNote, setReportNote] = useState<string>('')
   const [selectedEmployee, setSelectedEmployee] = useState<string>('')
+  const [alertMessage, setAlertMessage] = useState<string>('')
+  const [errorMessage, setErrorMessage] = useState<string>('')
 
   const fetchOrders = async () => {
     try {
@@ -62,6 +68,22 @@ const Reservation: React.FC<ReservationProps> = ({
     }
   }, [reservation.orders])
 
+  //@todo split into validation and submit; change alert into errorMes
+
+  const validate = () => {
+    if (!reportType) {
+      setErrorMessage('Please select a valid report type.')
+      return false
+    } else if (!reportNote) {
+      setErrorMessage('Please tell us what happened.')
+      return false
+    } else if (reportType === 'complain-employee') {
+      setErrorMessage('Please select an employee to report.')
+      return false
+    }
+    return true
+  }
+
   const handleReportSubmit = async () => {
     try {
       let endpoint = ''
@@ -82,10 +104,6 @@ const Reservation: React.FC<ReservationProps> = ({
           endpoint = '/reports/report-order'
           break
         case 'complain-employee':
-          if (!selectedEmployee) {
-            alert('Please select an employee to report.')
-            return
-          }
           endpoint = '/reports/report-employee'
           reportData = {
             ...reportData,
@@ -93,14 +111,13 @@ const Reservation: React.FC<ReservationProps> = ({
           }
           break
         default:
-          alert('Please select a valid report type.')
           return
       }
 
       console.log(reportData)
 
       await fetchPOST(endpoint, JSON.stringify(reportData))
-      alert('Your report has been submitted successfully.')
+      setAlertMessage('Your report has been submitted successfully.')
     } catch (error) {
       if (error instanceof FetchError) console.error(error.formatErrors())
     } finally {
@@ -115,9 +132,10 @@ const Reservation: React.FC<ReservationProps> = ({
 
   const allReservationEmployees = () => {
     let res: UserType[] = []
+    console.log(reservation)
     for (let index = 0; index < orders.length; index++) {
       const employees: UserType[] = orders[index].employees
-      for (let index = 0; index < employees.length; index++) {
+      for (let index = 0; index < employees?.length; index++) {
         const emp = employees[index]
         if (!res.find(e => e.userId === emp.userId)) res.push(emp)
       }
@@ -193,7 +211,10 @@ const Reservation: React.FC<ReservationProps> = ({
           </div>
           <Dialog
             open={isComplaining}
-            onClose={() => setIsComplaining(false)}
+            onClose={() => {
+              setIsComplaining(false)
+              setErrorMessage('')
+            }}
             title="Make a complaint"
           >
             <div className="flex flex-col gap-4 dark:text-grey-1 p-4 w-[400px]">
@@ -269,9 +290,12 @@ const Reservation: React.FC<ReservationProps> = ({
                 placeholder="Describe your issue in detail..."
                 className="border-[1px] rounded-md p-2 h-20 scroll dark:text-grey-1 dark:bg-black"
               />
-
+              {errorMessage && <ErrorMes msg={errorMessage} />}
               <button
-                onClick={handleReportSubmit}
+                type="submit"
+                onClick={() => {
+                  if (validate()) handleReportSubmit()
+                }}
                 className="bg-primary text-white rounded-md p-2 transition hover:bg-secondary"
               >
                 Submit Report
@@ -279,6 +303,28 @@ const Reservation: React.FC<ReservationProps> = ({
             </div>
           </Dialog>
         </>
+      )}
+      {alertMessage && (
+        <div className="fixed bottom-2 left-2">
+          <Alert
+            variant="filled"
+            severity="success"
+            action={
+              <IconButton
+                aria-label="close"
+                color="inherit"
+                size="small"
+                onClick={() => {
+                  setAlertMessage('')
+                }}
+              >
+                <CloseIcon fontSize="inherit" />
+              </IconButton>
+            }
+          >
+            {alertMessage}
+          </Alert>
+        </div>
       )}
     </div>
   )
