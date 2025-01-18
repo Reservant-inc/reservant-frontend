bbe0ca2c37b5f1fe6e7c1e1abdca262462df2fa9
Miriam Staśkiewicz <miriam@MacBook-Air-Miriam.local>
2025-01-18 20:27:13 +0100

    #242

--- src/components/reservant/profile/Account.tsx
+++ src/components/reservant/profile/Account.tsx
@@ -76,10 +76,11 @@ const Account: React.FC = () => {
   const [t] = useTranslation('global')
 
   const initialValues = {
-    phoneNumber: userInfo.phoneNumber?.number,
+    phoneNumber: `${userInfo.phoneNumber?.code || ''}${userInfo.phoneNumber?.number || ''}`,
     firstName: userInfo.firstName,
     lastName: userInfo.lastName
   }
+  
 
   const passValues = {
     oldPassword: '',
@@ -164,24 +165,29 @@ const Account: React.FC = () => {
 
   const updateUserData = async (userData: any) => {
     try {
+      const phoneNumber = userData.phoneNumber || '';
+      const code = phoneNumber.slice(0, 3); // Pobiera pierwsze trzy znaki jako kod kraju
+      const number = phoneNumber.slice(3); // Pobiera resztę numeru
+  
       await fetchPUT(
         '/user',
         JSON.stringify({
-          phoneNumber: userData.phoneNumber,
+          phoneNumber: { code, number },
           firstName: userData.firstName,
           lastName: userData.lastName,
           birthDate: '1999-12-31',
           photo: photoFileName
         })
-      )
+      );
     } catch (error) {
-      if (error instanceof FetchError) console.log(error.formatErrors())
-      else console.log(error)
+      if (error instanceof FetchError) console.log(error.formatErrors());
+      else console.log(error);
     } finally {
-      setIsEditing(false)
-      fetchUserData()
+      setIsEditing(false);
+      fetchUserData();
     }
-  }
+  };
+  
   const updatePass = async (values: FormikValues) => {
     try {
       await fetchPOST(
