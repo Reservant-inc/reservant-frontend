5a8e3d791c07b29b1b66c7196283bd16e659b0a4
s26496 <s26496@pjwstk.edu.pl>
2025-01-22 00:10:55 +0100

    cant ban BOK manager snackbar #397
    - i przeróbka FetchError bo coś się w backendzie zmieniło i sypało błędami

--- src/services/Errors.tsx
+++ src/services/Errors.tsx
@@ -6,13 +6,25 @@ export class FetchError extends Error {
     super(message)
     this.name = 'FetchError'
     this.status = status
-    this.errors = errors[''].map((error: string) => `${error}`).join('\n')
+    this.errors = Array.isArray(errors?.[''])
+      ? errors[''].map((error: string) => `${error}`)
+      : []; 
+    // this.errors = errors[''].map((error: string) => `${error}`).join('\n')
   }
 
   formatErrors(): string {
-    const message: string = this.errors['']
+    if (!Array.isArray(this.errors) || this.errors.length === 0) {
+      return `Error code: ${this.status}, No additional error details provided.`;
+    }
+
+    const message: string = this.errors
       .map((error: string) => `Error: ${error}`)
-      .join('\n')
+      .join('\n');
+
+  // formatErrors(): string {
+  //   const message: string = this.errors['']
+  //     .map((error: string) => `Error: ${error}`)
+  //     .join('\n')
 
     return 'Error code: ' + this.status + ', ' + message
   }
