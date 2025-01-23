f3d6da5339cca6d96621590d08c5b2c0b56f5afa
Jarosław Gawrych <jaroslaw.gawrych@gmail.com>
2025-01-23 20:30:01 +0100

    addded report information

--- src/components/reservant/restaurantManagement/Reports.tsx
+++ src/components/reservant/restaurantManagement/Reports.tsx
@@ -34,6 +34,7 @@ export default function Reports() {
     try {
       const response = await fetchGET(`/my-restaurants/${restaurantId}/reports`)
 
+      console.log(response)
       let reports: any[] = []
 
       if (response.items.length)
@@ -43,11 +44,33 @@ export default function Reports() {
             reportId: response.items[i].reportId,
             description: response.items[i].description,
             visitId: response.items[i].visit.visitId,
-            reportedUserId: response.items[i].reportedUser?.userId,
             reportDate: new Date(response.items[i].reportDate).toLocaleString(),
-            category: response.items[i].category,
-            createdBy: response.items[i].createdBy,
-            reportedUser: response.items[i].reportedUser
+            category:
+              response.items[i].category === 'LostItem'
+                ? t('profile.reports.category.lost')
+                : response.items[i].category === 'RestaurantEmployeeReport'
+                  ? t('profile.reports.category.emp')
+                  : response.items[i].category === 'Technical'
+                    ? t('profile.reports.category.technical')
+                    : response.items[i].category,
+            // createdBy: response.items[i].createdBy,
+            reportedEmployee: response.items[i].reportedUser
+              ? response.items[i].reportedUser.firstName +
+                ' ' +
+                response.items[i].reportedUser.lastName
+              : '',
+            resolutionComment: response.items[i].resolutionComment,
+            // resolvedBy: response.items[i].resolvedBy,
+            resolutionDate: response.items[i].resolvedBy,
+            // assignedAgents: response.items[i].assignedAgents,
+            reportStatus:
+              response.items[i].reportStatus === 'ResolvedNegatively'
+                ? t('profile.reports.status.rejected')
+                : response.items[i].reportStatus === 'NotResolved'
+                  ? t('profile.reports.status.pending')
+                  : t('profile.reports.status.resolved')
+
+            // threadId: response.items[i].threadId
           })
         }
       setRows(reports)
@@ -85,24 +108,24 @@ export default function Reports() {
       editable: false
     },
     {
+      field: 'reportStatus',
+      headerName: t('customer-service.reports.status.status'),
+      type: 'string',
+      flex: 0.1,
+      editable: false
+    },
+    {
+      field: 'category',
+      headerName: t('customer-service.reports.category'),
+      type: 'string',
+      flex: 0.1,
+      editable: false
+    },
+    {
       field: 'description',
       headerName: t('customer-service.reports.description'),
       type: 'string',
-      flex: 0.3,
-      editable: false
-    },
-    {
-      field: 'visitId',
-      headerName: t('customer-service.reports.visit-id'),
-      type: 'string',
-      flex: 0.05,
-      editable: false
-    },
-    {
-      field: 'reportedUserId',
-      headerName: t('customer-service.reports.reported-user-id'),
-      type: 'string',
-      flex: 0.2,
+      flex: 0.1,
       editable: false
     },
     {
@@ -113,10 +136,31 @@ export default function Reports() {
       editable: false
     },
     {
-      field: 'category',
-      headerName: t('customer-service.reports.category'),
+      field: 'visitId',
+      headerName: t('customer-service.reports.visit-id'),
       type: 'string',
-      flex: 0.2,
+      flex: 0.05,
+      editable: false
+    },
+    {
+      field: 'reportedEmployee',
+      headerName: t('customer-service.reports.reported-employee'),
+      type: 'string',
+      flex: 0.1,
+      editable: false
+    },
+    {
+      field: 'resolutionDate',
+      headerName: t('customer-service.reports.resolution-date'),
+      type: 'string',
+      flex: 0.1,
+      editable: false
+    },
+    {
+      field: 'resolutionComment',
+      headerName: t('customer-service.reports.resolution-note'),
+      type: 'string',
+      flex: 0.1,
       editable: false
     }
   ]
