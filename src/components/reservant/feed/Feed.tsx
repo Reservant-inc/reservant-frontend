5e951c183fc174c92ccdc17a607a07aa6608e577
KubaBili001 <kubabili.wawa@gmail.com>
2025-02-06 18:21:02 +0100

    feed screen styling

--- src/components/reservant/feed/Feed.tsx
+++ src/components/reservant/feed/Feed.tsx
@@ -7,12 +7,12 @@ const Feed: React.FC = () => {
   const [t] = useTranslation('global')
 
   return (
-    <div className="w-full min-h-screen flex flex-col bg-grey-1 p-2 dark:bg-grey-6">
+    <div className="w-full min-h-screen flex flex-col bg-white p-2 dark:bg-black">
       <div className="flex flex-col items-center w-full">
         <div className="w-[800px] text-xl py-2 text-center dark:text-grey-1">
           {t('profile.events.events-feed')}
         </div>
-        <div className="w-[800px] bg-white rounded-lg p-4 dark:bg-black dark:text-grey-1">
+        <div className="w-[800px] bg-white rounded-lg p-4 dark:bg-black dark:text-grey-1 border-[1px] dark:border-white border-black">
           <EventList listType={EventListType.History} />
         </div>
       </div>
